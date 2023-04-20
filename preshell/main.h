7415ee2cd602fe593322b3f15aaf8372e028b192
solp22 <sol.puentereal@gmail.com>
2023-04-20 05:45:14 -0700

    preshell tasks

---
+++ preshell/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**libraries**/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+/**prototypes**/
+#endif
+
