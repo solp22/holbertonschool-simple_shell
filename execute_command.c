57704cb77b7902b42b34f5f5a9e01e1cb1601451
solp22 <sol.puentereal@gmail.com>
2023-04-22 06:46:05 -0700

    super simple shell execute command error fix

--- execute_command.c
+++ execute_command.c
@@ -2,19 +2,11 @@
 
 void execute_command(char **array, char *token)
 {
-    char *command = NULL;
     pid_t childpid = fork();
 
     if (childpid == 0)
     {
-        printf("a");
-        sprintf(command, "%s%s", "/usr/bin/", token);
-        if (access(command, F_OK) == -1)
-        {
-            perror("Error:");
-            exit(1);
-        }
-        if (execve(command, array, NULL) == -1)
+        if (execve(array[0], array, NULL) == -1)
         {
             perror("Error:");
         }
